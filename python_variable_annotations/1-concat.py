e6a711e3f1f89dad988df56817859407de230e88
sa3ina <sravnennaya@mail.ru>
2026-02-15 21:26:56 +0400

    new task

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to concat strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenated string"""
+    return str1 + str2
