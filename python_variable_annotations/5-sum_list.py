eccbe6ad7d08a237881a2579bdfc25d6255a4de9
sa3ina <sravnennaya@mail.ru>
2026-02-15 21:48:16 +0400

    new task

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that takes list of floats and returns the sum as float"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of a list of floats"""
+    return sum(input_list)
