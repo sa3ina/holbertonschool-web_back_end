2f377157647ebd72f55ac3ce8f746d4f3d2c219f
sa3ina <sravnennaya@mail.ru>
2026-02-16 17:34:17 +0400

    new task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return the list of delays"""
     delays = [await task_wait_random(max_delay) for _ in range(n)]
     return sorted(delays)
