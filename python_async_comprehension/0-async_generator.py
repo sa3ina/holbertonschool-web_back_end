5186eee38d4358beb398f57069d9a65685eafcdc
sa3ina <sravnennaya@mail.ru>
2026-02-17 15:47:08 +0400

    new task added

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module providing coroutine that takes no arguments
+will loop 10 times, each time async. wait 1 second
+then yield a random number between 0 and 10
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Wait 1 second,return
+    random number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        i = random.uniform(0, 10)
+        yield i
