bd3fdc9d4b8022a404b760d0bc2d844b22e6092e
sa3ina <sravnennaya@mail.ru>
2026-02-17 16:16:36 +0400

    new task added

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that imports function
+the coroutine collect 10 random numbers
+and return it
+"""
+
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return collected 10 random numbers"""
+    return [number async for number in async_generator()]
