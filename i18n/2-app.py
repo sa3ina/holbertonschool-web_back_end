56bd53b1f409bce09373b2d761bec8f49a636244
sa3ina <sravnennaya@mail.ru>
2026-04-07 23:27:09 +0400

    -

---
+++ i18n/2-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Flask-Babel
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+def get_locale():
+    """Determines the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(locale_selector=get_locale)
+babel.init_app(app)
+
+
+@app.route("/")
+def home():
+    """Route that returns a template"""
+    return render_template('2-index.html')
