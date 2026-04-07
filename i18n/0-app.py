35f2e21783865add23c9695c62ae0d9867fe76f2
sa3ina <sravnennaya@mail.ru>
2026-04-07 22:54:01 +0400

    -

---
+++ i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic Flask application"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Route that returns a template"""
+    return render_template('0-index.html')
