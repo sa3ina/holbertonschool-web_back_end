74170243f2beafce9ee7ee62133307dd2da622aa
sa3ina <sravnennaya@mail.ru>
2026-04-08 16:18:56 +0400

    -

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python3
-"""
-Babel object instantiation
-"""
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
 
 class Config:
-    """
-    Configuring the languages
-    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
+app.config.from_object(Config)
 
 def get_locale():
-    """
-    Determines the best match for supported languages
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app, locale_selector=get_locale)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    """
-    Renders the template
-    """
-    return render_template('3-index.html')
+    return render_template("3-index.html")
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
