11da341339a05cb515fab4e89b7e06beea611545
sa3ina <sravnennaya@mail.ru>
2026-05-02 23:35:08 +0400

    redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,12 +4,13 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
+
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
@@ -18,3 +19,20 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
