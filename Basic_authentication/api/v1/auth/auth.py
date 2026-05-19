aa1d6a593a0f095dac209e3ef0c7c0fc4038349a
sa3ina <sravnennaya@mail.ru>
2026-05-19 19:52:51 +0400

    basic auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Auth class """
+
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """
+        Returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None
+        """
+        return None
