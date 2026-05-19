3440b9ee0340c4535857be3b89fd6c2e42f03f0f
sa3ina <sravnennaya@mail.ru>
2026-05-19 19:59:39 +0400

    slash tolerance fix

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,33 +1,38 @@
-from typing import List, TypeVar
+#!/usr/bin/env python3
+'''
+manage the API authentification
+'''
 from flask import request
+from typing import TypeVar, List
 
 
-class Auth:
-    """ Auth class """
-
+class Auth():
+    '''
+    manage the API authentification
+    '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Returns True if authentication is required, otherwise False
-        """
-
+        ''' require_authentification '''
         if path is None:
             return True
-
-        if excluded_paths is None or excluded_paths == []:
+        if excluded_paths is None:
             return True
-
-        # normalize path (slash tolerant)
-        if not path.endswith('/'):
-            path += '/'
-
-        for excluded_path in excluded_paths:
-            if excluded_path == path:
-                return False
-
+        if len(excluded_paths) == 0:
+            return True
+        if path is None or excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        return None
+        ''' def authorization_header '''
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
+        ''' def current_user '''
         return None
