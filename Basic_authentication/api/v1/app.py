28772d245ce4f6c4d9302a60d63003a9fe40a5c3
sa3ina <sravnennaya@mail.ru>
2026-05-19 20:18:48 +0400

    basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -17,7 +17,10 @@ auth = None
 
 AUTH_TYPE = getenv("AUTH_TYPE")
 
-if AUTH_TYPE == "auth":
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
