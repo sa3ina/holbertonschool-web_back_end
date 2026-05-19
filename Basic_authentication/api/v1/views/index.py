9c401744a071d594ec1b227eebe2420c7f629150
sa3ina <sravnennaya@mail.ru>
2026-05-19 19:45:42 +0400

    basic auth

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized():
         401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Raises:
+        403 error
+    """
+    abort(403)
