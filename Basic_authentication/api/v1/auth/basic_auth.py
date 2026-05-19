d793d3f95ad84e5860f9d57125ff5322d0fb3496
sa3ina <sravnennaya@mail.ru>
2026-05-19 20:39:07 +0400

    basic auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-"""
-BasicAuth module
-"""
+''' Module of Basic_auth
+'''
+import base64
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class """
-
-
-def extract_base64_authorization_header(
-        self, authorization_header: str
-) -> str:
-    """
-    Returns the Base64 part of the Authorization header
-    """
-
-    if authorization_header is None:
-        return None
-
-    if not isinstance(authorization_header, str):
-        return None
-
-    if not authorization_header.startswith("Basic "):
-        return None
-
-    parts = authorization_header.split("Basic ")
-    return parts[1]
+    ''' BasicAuth class
+    '''
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        ''' def extract base64 authorization header '''
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith("Basic "):
+            return "".join(authorization_header.split(" ")[1:])
