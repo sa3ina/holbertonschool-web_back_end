b713ca69400d34aa001bdb197d8075c4d9969986
sa3ina <sravnennaya@mail.ru>
2026-05-20 10:59:16 +0400

    fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,10 +11,11 @@ from models.user import User
 class BasicAuth(Auth):
     """BasicAuth class"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str
-                                            ) -> str:
-        """def extract base64 authorization header"""
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+    ) -> str:
+        """Extract base64 part of Authorization header"""
 
         if authorization_header is None:
             return None
@@ -28,9 +29,10 @@ class BasicAuth(Auth):
         return authorization_header.split(" ")[1]
 
     def decode_base64_authorization_header(
-        self, base64_authorization_header: str
+            self,
+            base64_authorization_header: str
     ) -> str:
-        """def decode base64 authorization header"""
+        """Decode base64 authorization header"""
 
         if base64_authorization_header is None:
             return None
@@ -45,9 +47,10 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-        self, decoded_base64_authorization_header: str
+            self,
+            decoded_base64_authorization_header: str
     ) -> (str, str):
-        """def extract user credentials"""
+        """Extract user email and password"""
 
         if decoded_base64_authorization_header is None:
             return None, None
@@ -58,6 +61,34 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return None, None
 
-        credentials = decoded_base64_authorization_header.split(":", 1)
+        email, password = decoded_base64_authorization_header.split(":", 1)
 
-        return credentials[0], credentials[1]
+        return email, password
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+    ) -> TypeVar('User'):
+        """Get User instance from credentials"""
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
