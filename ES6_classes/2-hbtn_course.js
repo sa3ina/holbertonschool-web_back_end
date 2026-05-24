3e4b6e5c2cbfa10b6f5889b966fbd3b2accdd441
sa3ina <sravnennaya@mail.ru>
2026-05-24 18:26:48 +0400

    es6 classes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students array must contain only strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(x) {
+    if (typeof x !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = x;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(x) {
+    if (typeof x !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = x;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(x) {
+    if (!x.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students array must contain only strings');
+    }
+    this._students = x;
+  }
+}
---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(x) {
+    this._amount = x;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(x) {
+    this._currency = x;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
