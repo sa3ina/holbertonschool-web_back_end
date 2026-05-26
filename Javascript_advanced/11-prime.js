4312825ffb4e1eace272f471c07f574eba7d8cf7
sa3ina <sravnennaya@mail.ru>
2026-05-26 22:47:32 +0400

    checker fix

--- Javascript_advanced/11-prime.js
+++ Javascript_advanced/11-prime.js
@@ -1,30 +1,23 @@
 function countPrimeNumbers() {
-    let count = 0;
-
+    let counter = 0;
     for (let i = 2; i <= 100; i++) {
-        let isPrime = true;
+        let flag = 0;
 
-        for (let j = 2; j * j <= i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
+        for (let x = 2; x < i; x++) {
+            if (i % x === 0) {
+                flag = 1;
                 break;
             }
         }
+        if (i > 1 && flag == 0)
+            counter += 1;
+    };
+    return counter;
+};
 
-        if (isPrime) {
-            count++;
-        }
-    }
-
-    return count;
+const startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
 }
-
-let startTime = performance.now();
-
-setTimeout(function () {
-    for (let i = 0; i < 100; i++) {
-        countPrimeNumbers();
-    }
-    let endTime = performance.now();
-    console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
-}, 0);
+const endTime = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds`);
