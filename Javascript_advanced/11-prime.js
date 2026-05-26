ea2a98430bb4d5a918c574ed412681b726a8b6ff
sa3ina <sravnennaya@mail.ru>
2026-05-26 22:50:48 +0400

    task 11

--- Javascript_advanced/11-prime.js
+++ Javascript_advanced/11-prime.js
@@ -28,5 +28,5 @@ setTimeout(() => {
     }
 
     const end = performance.now();
-    console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds`);
+    console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`);
 }, 0);
